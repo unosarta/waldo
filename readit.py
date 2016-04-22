af692b28c4a8eb54a1b53a4102be3248368814d5
Jared Pettingill <sen.hashi.tengoku.ni@gmail.com>
2016-04-21 21:00:53 -0500

    edited rob's code with my own solution to data structure design

--- readit.py
+++ readit.py
@@ -21,3 +21,22 @@ def readit(filename):
 		else: line = data.readline() 
 	return event_log
 
+def file_struct(event_log):
+
+	ready_for_vtk = {}
+	for particle in event_log:
+		ready_for_vtk[particle] = []
+		for event in event_log[particle]:
+			list = event.split()
+			ready_for_vtk[particle].append({})
+			ready_for_vtk[particle][event_log[particle].index(event)]['something'] = list[0]
+			ready_for_vtk[particle][event_log[particle].index(event)]['cell'] = list[1]
+			ready_for_vtk[particle][event_log[particle].index(event)]['x'] = list[2]
+			ready_for_vtk[particle][event_log[particle].index(event)]['y'] = list[2]
+			ready_for_vtk[particle][event_log[particle].index(event)]['z'] = list[3]
+			ready_for_vtk[particle][event_log[particle].index(event)]['u'] = list[4]
+			ready_for_vtk[particle][event_log[particle].index(event)]['v'] = list[5]
+			ready_for_vtk[particle][event_log[particle].index(event)]['w'] = list[6]
+			ready_for_vtk[particle][event_log[particle].index(event)]['erg'] = list[7]
+			ready_for_vtk[particle][event_log[particle].index(event)]['wgt'] = list[8]
+	return ready_for_vtk
