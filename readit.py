1801cbd23857d6d3029742dd9dd7e0314b6cf802
Jared Pettingill <sen.hashi.tengoku.ni@gmail.com>
2016-04-26 14:25:28 -0500

    added code to create VTK file, visIt can read but does not plot the VTK - to be fixed

--- readit.py
+++ readit.py
@@ -1,4 +1,5 @@
 import pdb
+import itertools as it
 def readit(filename):
 
 	data = open(filename, 'r+')
@@ -40,3 +41,26 @@ def file_struct(event_log):
 			ready_for_vtk[particle][event_log[particle].index(event)]['erg'] = list[7]
 			ready_for_vtk[particle][event_log[particle].index(event)]['wgt'] = list[8]
 	return ready_for_vtk
+
+def vtk_file(events, event_title):
+
+	file_name = event_title + ".vtk"
+	vtk_file = open(file_name,"w+")
+	num_events = 0
+	for event in events:
+		if event["cell"] != events[events.index(event)-1]["cell"]:
+			num_events += 1
+	vtk_file.write("# vtk DataFile Version 3.0 \nvtk output\nASCII\nDATASET POLYDATA\nPOINTS " + str(num_events) + " float\n")
+	for event in events:
+		if event["cell"] != events[events.index(event)-1]["cell"]:
+			vtk_file.write(event["x"] + " " + event["y"] + " " + event["z"] + "\n")
+	num_lines = num_events - 1
+	vtk_file.write("LINES " + str(num_lines) + " " + str(3*num_lines) + "\n")
+	for i in range(num_events-1):
+		vtk_file.write("2 " + str(i) + " " + str(i+1) + "\n")
+	vtk_file.write("CELL_DATA " + str(num_lines) + "\n")
+	vtk_file.write("scalars cellvar float\nLOOKUP_TABLE default\n")
+	vtk_file.write("2.0 2.4 2.1 2.2 2.3\n")
+	vtk_file.write("POINT_DATA " + str(num_events) + "\n")
+	vtk_file.write("scalars pointvar float\nLOOKUP_TABLE default\n")
+	vtk_file.write("1.2 1.3 1.4 1.5")
